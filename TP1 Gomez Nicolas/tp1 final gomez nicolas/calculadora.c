3603de968450db992ad24e0dcda69e19d2fb08ca
nicolasnet <niconetgomez@hotmail.com>
2017-09-06 17:37:17 -0300

    TP1 calculadora, cerca del final
    Documentadas las funciones, cambio de variables a tipo float para soportar nros reales, agregado bloque de codigo del esqueleto de ejemplo para la compatibilidad e inclusion de bibliotecas en todos los archivos del proyecto

--- TP1 Gomez Nicolas/tp1 final gomez nicolas/calculadora.c
+++ TP1 Gomez Nicolas/tp1 final gomez nicolas/calculadora.c
@@ -1,34 +1,43 @@
-int suma (int numero1, int numero2)
+#ifndef FUNCIONES_H_INCLUDED  //revisar funcionamiento de este bloque de codigos, sirve para vincular y q no se dupliquen las bibliotecas del proyecto
+#define FUNCIONES_H_INCLUDED
+
+
+
+#endif // FUNCIONES_H_INCLUDED
+
+
+float suma (float numero1, float numero2)
 {
-    int resultado;
+    float resultado;
 
     resultado=(numero1+numero2);
     return resultado;
 }
 
 
-int resta (int numero1, int numero2)
+float resta (float numero1, float numero2)
 {
-    int resultado;
+    float resultado;
 
     resultado=(numero1-numero2);
     return resultado;
 }
 
 
-int multiplicacion (int numero1, int numero2)
+float multiplicacion (float numero1, float numero2)
 {
-    int resultado;
+    float resultado;
 
     resultado=(numero1*numero2);
     return resultado;
 }
 
 
-float division (int numero1, int numero2)
+float division (float numero1, float numero2)
 {
     float resultado;
-    resultado= (float) numero1/numero2;
+
+    resultado= numero1/numero2;
     return resultado;
 }
 
--- TP1 Gomez Nicolas/tp1 final gomez nicolas/calculadora.h
+++ TP1 Gomez Nicolas/tp1 final gomez nicolas/calculadora.h
@@ -1,13 +1,52 @@
 /** \brief  esta funcion suma 2 (dos) operandos.
  *
- * \param
- * \param
- * \return
+ * \param Operando numero1, variable del tipo float (flotante).
+ * \param Operando numero2, variable del tipo float (flotante).
+ * \return Valor del tipo float (flotante). El cual es la suma de los dos operando ingresados en la funcion.
  *
  */
-int suma (int, int);
-int resta (int, int);
-int multiplicacion (int, int);
-float division (int, int);
-int factorial(int);
+float suma (float, float);
 
+
+
+/** \brief  esta funcion resta 2 (dos) operandos.
+ *
+ * \param Operando numero1, variable del tipo float (flotante).
+ * \param Operando numero2, variable del tipo float (flotante).
+ * \return Valor del tipo float (flotante). El cual es la resta del operando numero1 con el operando numero2.
+ *
+ */
+float resta (float, float);
+
+
+
+/** \brief  esta funcion multiplica 2 (dos) operandos.
+ *
+ * \param Operando numero1, variable del tipo float (flotante).
+ * \param Operando numero2, variable del tipo float (flotante).
+ * \return Valor del tipo float (flotante). El cual es el producto del operando numero1 con el operando numero2.
+ *
+ */
+float multiplicacion (float, float);
+
+
+
+/** \brief  esta funcion divide 2 (dos) operandos.
+ *
+ * \param Operando numero1, variable del tipo float (flotante).
+ * \param Operando numero2, variable del tipo float (flotante).
+ * \return Valor del tipo float (flotante). El cual es el cociente entre operando numero1(dividendo) con el operando numero2(divisor).
+ *
+ */
+float division (float, float);
+
+
+
+
+/** \brief  esta funcion calcula el factorial de un operando.
+ *
+ * \param Operando numero1, variable del tipo int (entero). Numeros naturales positivos (no incluido el cero).
+ * \return Valor del tipo int (entero). Se obtiene el factorial del operando numero1 ingresado. Es el producto de todos los numeros naturales empezando por 1(uno) hasta el operando numero1 ingresado.
+ *
+ */
+int factorial(int);
